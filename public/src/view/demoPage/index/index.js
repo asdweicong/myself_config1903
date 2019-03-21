3bce86b093fb70981f338f4582ecc11c51c220ea
weicong <weicong@weicongdeiMac.local>
2019-03-21 10:59:27 +0800

    add

--- public/src/view/demoPage/index/index.js
+++ public/src/view/demoPage/index/index.js
@@ -1,6 +1,5 @@
 
-// require('../../../bower_components/avalon/avalon.2.2.9');
-// require('../../../bower_components/avalon/deferred');
+require('../../../bower_components/avalon/deferred');
 require('../../../lib/common.css');
 
 require('./controller');
